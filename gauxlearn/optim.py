a55138594f73bdeecdb2a9c98ea03e62f19e9387
Daonan Zhang <76598884+DaonanZhang@users.noreply.github.com>
2024-01-19 14:29:18 +0100

    Update optim.py
    some comments

--- gauxlearn/optim.py
+++ gauxlearn/optim.py
@@ -41,7 +41,13 @@ class MetaOptimizer:
         for p, g in zip(aux_params, hyper_gards):
             p.grad = g
 
-        # grad clipping
+        # grad clipping makes the normlization
+
+# aux_params：这是包含模型参数的列表或张量。通常，这是模型的权重和偏置参数。
+# max_norm：这是梯度的最大范数（norm）的阈值。梯度的范数是梯度向量的长度。
+# clip_grad_norm_ 函数的作用是计算模型的梯度向量的范数，并将其裁剪到不超过 max_norm 的大小。如果梯度的范数大于 max_norm，则梯度向量会被重新缩放，使其不超过这个阈值，以防止梯度爆炸的发生。
+# 梯度剪裁有助于提高深度学习模型的稳定性，减少训练过程中的梯度问题，并更容易使模型收敛到合适的解。
+# 这对于训练深层神经网络非常有用，特别是在使用循环神经网络（RNN）等容易受到梯度爆炸问题影响的模型时。通过设置合适的 max_grad_norm 阈值，可以控制梯度剪裁的程度。
         if self.max_grad_norm is not None:
             clip_grad_norm_(aux_params, max_norm=self.max_grad_norm)
 
